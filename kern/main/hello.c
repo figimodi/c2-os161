0e090a32b1fb11d5718cc1b907100afc70f48e8a
g-orl <peppeorlando2000@gmail.com>
2023-05-17 16:37:07 +0200

    Initial commit

---
+++ kern/main/hello.c
@@ -0,0 +1,24 @@
+#include <types.h>
+#include <kern/errno.h>
+#include <kern/reboot.h>
+#include <kern/unistd.h>
+#include <lib.h>
+#include <spl.h>
+#include <clock.h>
+#include <thread.h>
+#include <proc.h>
+#include <current.h>
+#include <synch.h>
+#include <vm.h>
+#include <mainbus.h>
+#include <vfs.h>
+#include <device.h>
+#include <syscall.h>
+#include <test.h>
+#include <version.h>
+#include "autoconf.h"  // for pseudoconfig
+#include "hello.h"
+
+void hello (void){
+  kprintf("HELLO\n");
+}
